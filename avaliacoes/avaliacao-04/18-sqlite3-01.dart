7b41b1b10415e99522077e03c584927464e4816e
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2024-12-02 22:50:34 -0300

    Update 18-sqlite3-01.dart

--- avaliacoes/avaliacao-04/18-sqlite3-01.dart
+++ avaliacoes/avaliacao-04/18-sqlite3-01.dart
@@ -1,18 +1,11 @@
-// Exemplo do Pacote sqlite3 2.1.0
-//
-
 import 'dart:io';
-
 import 'package:sqlite3/sqlite3.dart';
 
 void main() {
   print('Using sqlite3 ${sqlite3.version}');
 
-  // Create a new in-memory database. To use a database backed by a file, you
-  // can replace this with sqlite3.open(yourFilePath).
   final db = sqlite3.openInMemory();
 
-  // Create a table and insert some data
   db.execute('''
     CREATE TABLE alunos (
       id INTEGER NOT NULL PRIMARY KEY,
@@ -20,27 +13,20 @@ void main() {
     );
   ''');
 
-  // Prepare a statement to run it multiple times:
   final stmt = db.prepare('INSERT INTO alunos (name) VALUES (?)');
   stmt
     ..execute(['Matheus Pereira'])
     ..execute(['Beatriz de Oliveira']);
 
-  // Dispose a statement when you don't need it anymore to clean up resources.
   stmt.dispose();
-
-  // You can run select statements with PreparedStatement.select, or directly
-  // on the database:
+  
   final ResultSet resultSet =
       db.select('SELECT * FROM alunos');
 
-  // You can iterate on the result set in multiple ways to retrieve Row objects
-  // one by one.
   for (final Row row in resultSet) {
     print('Aluno[id: ${row['id']}, name: ${row['name']}]');
   }
 
-  // Register a custom function we can invoke from sql:
   db.createFunction(
     functionName: 'dart_version',
     argumentCount: const AllowedArgumentCount(0),
@@ -48,6 +34,5 @@ void main() {
   );
   print(db.select('SELECT dart_version()'));
 
-  // Don't forget to dispose the database to avoid memory leaks
   db.dispose();
 }
