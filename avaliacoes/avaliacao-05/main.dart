a416f726419cf14de27fdccd26fef2118f0d41e7
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2024-12-12 17:58:23 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-05/main.dart
@@ -0,0 +1,25 @@
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+
+main() async {
+  // Configura as credenciais SMTP do Gmail
+  final smtpServer = gmail('', '');
+
+  // Cria uma mensagem de e-mail
+  final message = Message()
+    ..from = Address('', '')
+    ..recipients.add('')
+    ..subject = 'Atividade 05'
+    ..text = 'Email Dart SMTP';
+
+  try {
+    // Envia o e-mail usando o servidor SMTP do Gmail
+    final sendReport = await send(message, smtpServer);
+
+    // Exibe o resultado do envio do e-mail
+    print('E-mail enviado: ${sendReport.toString()}');
+  } on MailerException catch (e) {
+    // Exibe informações sobre erros de envio de e-mail
+    print('Erro ao enviar e-mail: ${e.toString()}');
+  }
+}
