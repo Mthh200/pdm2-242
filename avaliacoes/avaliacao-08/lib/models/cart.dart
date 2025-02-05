1891d61b157cb01f9bee395887345ea8c3d2a288
Matheus Pereira Menezes <124296642+Mthh200@users.noreply.github.com>
2025-02-04 21:50:15 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-08/lib/models/cart.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_shopper/models/cart.dart';
+
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart', style: Theme.of(context).textTheme.displayLarge),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: _CartList(),
+              ),
+            ),
+            const Divider(height: 4, color: Colors.black),
+            _CartTotal()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CartList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var itemNameStyle = Theme.of(context).textTheme.titleLarge;
+    var cart = context.watch<CartModel>();
+
+    return ListView.builder(
+      itemCount: cart.items.length,
+      itemBuilder: (context, index) => ListTile(
+        leading: const Icon(Icons.done),
+        trailing: IconButton(
+          icon: const Icon(Icons.remove_circle_outline),
+          onPressed: () {
+            cart.remove(cart.items[index]);
+          },
+        ),
+        title: Text(
+          cart.items[index].name,
+          style: itemNameStyle,
+        ),
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var hugeStyle =
+        Theme.of(context).textTheme.displayLarge!.copyWith(fontSize: 48);
+
+    return SizedBox(
+      height: 200,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<CartModel>(
+                builder: (context, cart, child) =>
+                    Text('\$${cart.totalPrice}', style: hugeStyle)),
+            const SizedBox(width: 24),
+            FilledButton(
+              onPressed: () {
+                var cart = context.read<CartModel>();
+                String message;
+
+                if (cart.items.isEmpty) {
+                  message = 'Your cart is empty. Add items to buy!';
+                } else {
+                  // Cria uma lista de strings para os itens e seus preços
+                  List<String> itemDetails = cart.items.map((item) {
+                    return '${item.name} - \$${item.price}';
+                  }).toList();
+
+                  // Junta os itens com " + " e calcula o total
+                  String itemsString = itemDetails.join(' + ');
+                  message = '$itemsString = Total \$${cart.totalPrice} - Feito por Matheus Pereira Menezes';
+                }
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(message)));
+              },
+              style: TextButton.styleFrom(foregroundColor: Colors.white),
+              child: const Text('BUY'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
